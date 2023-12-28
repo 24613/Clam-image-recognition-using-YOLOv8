4f7735ab17977522b3a6b20bd5e98e4622a6d9d7
24613 <155034117+24613@users.noreply.github.com>
2023-12-28 20:33:44 +0800

    Add files via upload

---
+++ 01_train_hah.py
@@ -0,0 +1,17 @@
+import multiprocessing                     #多執行緒套件
+from ultralytics import YOLO               #yolo套件
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()       #避免造成與主程式重複執行
+
+    model = YOLO('models/yolov8s.pt')      #載入育訓練模型
+
+    results = model.train(                 #訓練模型
+        data = "yaml/20231127_hah.yaml",   #指定訓練任務檔案
+        imgsz = 400,                       #輸入影像大小
+        epochs = 30,                       #訓練次數
+        patience = 50,                     #無改善結束訓練等待次數
+        batch = 1,                         #批次大小
+        project = 'yolov8s_hah',           #專案名稱
+        name = 'exp01'                     #訓練成果名稱
+    )
